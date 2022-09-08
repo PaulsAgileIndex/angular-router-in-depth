f2084750b7f7dfbb255290fd3182101b95282edb
frank <frank.sprich@gmail.com>
2022-09-08 14:31:40 +0200

    ...code along!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,28 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {AboutComponent} from './about/about.component';
+import {CourseComponent} from './courses/course/course.component';
 
 
 const routes: Routes = [
-
+  {
+    path: "courses",
+    component: CourseComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
