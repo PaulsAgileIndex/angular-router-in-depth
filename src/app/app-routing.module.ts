37f50bfc17e1cb8c61b4362b13d54ec524fc3c30
frank <frank.sprich@gmail.com>
2022-09-08 15:29:36 +0200

    ...from IDE - update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/r
 import {LoginComponent} from './login/login.component';
 import {AboutComponent} from './about/about.component';
 import {CourseComponent} from './courses/course/course.component';
+import {HomeComponent} from './courses/home/home.component';
 
-
+// Lazy loading of subcomponent tree via:
+// 1.) Dynamic import a file from the file system without extension (.ts) which will return a Promise:
+//            import('./courses/courses.module')
+// 2.) ...then load the module from the imported file with:
+//            .then(m => m.CoursesModule)
 const routes: Routes = [
   {
     path: "courses",
-    component: CourseComponent
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
   },
   {
     path: "about",
